a696c7927adcd28cab4f416968906bff124929cb
rafaDev <rrg1459@hotmail.com>
2024-01-31 16:22:51 -0400

    Add comments and reference

---
+++ Nuevos/fibo.rb
@@ -0,0 +1,56 @@
+
+# gem install benchmark
+require 'benchmark'
+
+# # forma 1
+@fibonacci_cache = {}
+
+def fibonacci1(n)
+  return n if n <= 1
+
+  # Si ya hemos calculado el número, devolvemos el resultado almacenado
+  return @fibonacci_cache[n] if @fibonacci_cache[n]
+
+  # Si no, calculamos el número y lo almacenamos en caché
+  result = fibonacci1(n-1) + fibonacci1(n-2)
+  @fibonacci_cache[n] = result
+
+  return result
+end
+
+
+# forma 2
+@cache = {}
+
+def fibonacci2(n)
+  if n < 2
+    n
+  elsif @cache[n]
+    @cache[n]
+  else
+    @cache[n] = fibonacci2(n - 1) + fibonacci2(n - 2)
+  end
+end
+
+
+# Ejemplo de uso
+puts "\nMethod 1"
+time1 = Benchmark.measure do
+  puts "fibonacci de #{6} #{fibonacci1(6)}" # Output: 8
+  puts "fibonacci de #{10} #{fibonacci1(10)}" # Output: 55
+  puts "fibonacci de #{15} #{fibonacci1(15)}" # Output: 610
+  puts "fibonacci de #{150} #{fibonacci1(150)}" # Output: 9969216677189303386214405760200
+  puts "fibonacci de #{1500} #{fibonacci1(1500)}" # Output: 13551125668563101951636936867148408377786010712418497242133543153221487310873528750612259354035717265300373778814347320257699257082356550045349914102924249595997483982228699287527241931811325095099642447621242200209254439920196960465321438498305345893378932585393381539093549479296194800838145996187122583354898000
+end
+
+puts "\nMethod 2"
+time2 = Benchmark.measure do
+  puts "fibonacci de #{6} #{fibonacci2(6)}" # Output: 8
+  puts "fibonacci de #{10} #{fibonacci2(10)}" # Output: 55
+  puts "fibonacci de #{15} #{fibonacci2(15)}" # Output: 610
+  puts "fibonacci de #{150} #{fibonacci2(150)}" # Output: 9969216677189303386214405760200
+  puts "fibonacci de #{1500} #{fibonacci2(1500)}" # Output: 13551125668563101951636936867148408377786010712418497242133543153221487310873528750612259354035717265300373778814347320257699257082356550045349914102924249595997483982228699287527241931811325095099642447621242200209254439920196960465321438498305345893378932585393381539093549479296194800838145996187122583354898000
+end
+
+puts "\nTime benchmark 1: #{time1.real}"
+puts "\nTime benchmark 2: #{time2.real}"
